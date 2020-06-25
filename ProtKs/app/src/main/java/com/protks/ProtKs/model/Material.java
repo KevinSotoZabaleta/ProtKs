aaa08d7a833d39d34965cb5db714610bb024ff42
KevinSotoZabaleta <67439225+KevinSotoZabaleta@users.noreply.github.com>
2020-06-25 18:11:18 -0400

    Creacion clase material

---
+++ ProtKs/app/src/main/java/com/protks/ProtKs/model/Material.java
@@ -0,0 +1,43 @@
+package com.protks.ProtKs.model;
+
+public class Material {
+
+    public String  id;
+    public String nombre;
+    public int precio;
+
+    public Material(String id, String nombre, int precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
