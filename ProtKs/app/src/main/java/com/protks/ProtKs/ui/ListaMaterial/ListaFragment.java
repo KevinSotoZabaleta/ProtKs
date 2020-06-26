a1c9b68043239c088665a366ae9818a0fe992958
KevinSotoZabaleta <67439225+KevinSotoZabaleta@users.noreply.github.com>
2020-06-26 17:11:39 -0400

    Creacion listaFragment

---
+++ ProtKs/app/src/main/java/com/protks/ProtKs/ui/ListaMaterial/ListaFragment.java
@@ -0,0 +1,38 @@
+package com.protks.ProtKs.ui.ListaMaterial;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.protks.ProtKs.R;
+
+public class ListaFragment extends Fragment {
+
+    private ListaViewModel mViewModel;
+
+    public static ListaFragment newInstance() {
+        return new ListaFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.lista_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ListaViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ ProtKs/app/src/main/java/com/protks/ProtKs/ui/ListaMaterial/ListaViewModel.java
@@ -0,0 +1,7 @@
+package com.protks.ProtKs.ui.ListaMaterial;
+
+import androidx.lifecycle.ViewModel;
+
+public class ListaViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
